94c61e04b259c2cc35515ba6afd60d4e1ceea919
Kathleen Ly <heykathl@users.noreply.github.com>
2022-06-13 22:04:07 +0100

    Amalgamated index and server.js - using commonJS

--- server/server.js
+++ server/server.js
@@ -1,19 +1,41 @@
-import express from "express";
-import cors from "cors";
-import choonify from "./routes/choonify"
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config()
+
+// Port
+const port = process.env.PORT || 5000;
 
 // Create server
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // Previous was bodyparser, server can read json
+// Start server
+app.listen(port, () => {
+  console.log(`Server started on PORT: ${port}`)
+})
+
+// URI
+// const URI = process.env.CHOONIFY_DB_URI
+
+// // Connect to MongoDB via Mongoose
+// mongoose
+//   .connect()
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => console.log(err.message));
 
 // Routes
-app.use("/", choonify)
-app.use("*", (req, res) => {
-  res.status(404).json({error: "Not Found"})
-});
+app.use("/choons", require('./routes/choons'))
 
-export default app
+// Middleware
+// app.use(cors());
+// app.use(express.json()); // Previous was bodyparser, server can read json
+
+// Routes
+// app.use("/", choonify)
+// app.use("*", (req, res) => {
+//   res.status(404).json({error: "Not Found"})
+// });
 
